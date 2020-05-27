f299044e46133313707f650ba7211f6c080270d8
Lucas Bruce <lucasmatheusbruce@gmail.com>
2020-05-27 17:28:48 -0300

    db_Biblioteca v0.1

--- db_Biblioteca.sql
+++ db_Biblioteca.sql
@@ -1,15 +1,28 @@
+CREATE DATABASE IF NOT EXISTS db_Biblioteca;
+SELECT DATABASE();
+USE db_Biblioteca;
+
 CREATE TABLE IF NOT EXISTS tbl_livro(
 	id_livro SMALLINT PRIMARY KEY AUTO_INCREMENT,
 	nome_livro VARCHAR(50) NOT NULL,
 	isbn CHAR(30),
-	id_autor SMALLINT NOT NULL,
 	data_publicacao DATE NOT NULL,
-	preco_livro DECIMAL NOT NULL
+	preco_livro DECIMAL NOT NULL,
+    id_autor SMALLINT NOT NULL,
+    id_editora SMALLINT NOT NULL,
+    CONSTRAINT fk_id_autor_tbl_livro FOREIGN KEY(id_autor)
+     REFERENCES tbl_autor(id_autor),
+	CONSTRAINT fk_id_editora_tbl_livro FOREIGN KEY(id_editora)
+     REFERENCES tbl_editora(id_editora)
 );
 
 CREATE TABLE IF NOT EXISTS tbl_autor(
 	id_autor SMALLINT PRIMARY KEY AUTO_INCREMENT,
-	nome_autor VARCHAR(50),
-	sobrenome_autor VARCHAR(60)
+	nome_autor VARCHAR(50) NOT NULL,
+	sobrenome_autor VARCHAR(60) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS tbl_editora(
+	id_editora SMALLINT PRIMARY KEY AUTO_INCREMENT,
+	nome_editora VARCHAR(50) NOT NULL
+);
